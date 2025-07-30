7ce838205547cf9130d177792c75556f06e5f20f
lalazlm <laura.zelma.alves@escola.pr.gov.br>
2025-07-30 08:22:40 -0300

    salvar

--- perguntas.js
+++ perguntas.js
@@ -1,15 +1,13 @@
 criaCartao(
     'Volei',
     'O que é o volei?',
-    'o voleibol é um desporto coletivo onde duas equipas de seis jogadores se enfrentam, tentando passar a bola por cima de uma rede 
-para que ela toque o chão do lado adversário, sem que a equipa contrária a consiga devolver'
+    'o voleibol é um desporto coletivo onde duas equipas de seis jogadores se enfrentam, tentando passar a bola por cima de uma rede para que ela toque o chão do lado adversário, sem que a equipa contrária a consiga devolver'
 )
 
 criaCartao(
     'Volei',
     'quais as regras do voleibol?',
-    'No vôlei, duas equipes de seis jogadores tentam passar a bola por cima da rede para fazer ela tocar o chão do lado adversário, 
-usando até três toques. Quem marcar mais pontos ganha o jogo'
+    'No vôlei, duas equipes de seis jogadores tentam passar a bola por cima da rede para fazer ela tocar o chão do lado adversário, usando até três toques. Quem marcar mais pontos ganha o jogo'
 )
 
 criaCartao(
