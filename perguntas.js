553321d404b6b7a3403cf4aafa37d4cfeae4f892
lalazlm <laura.zelma.alves@escola.pr.gov.br>
2025-08-20 08:11:50 -0300

    salvar

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,20 @@ criaCartao(
     'quais tipos de volei tem?',
     'o voleibol de quadra, o voleibol de praia e o voleibol sentado'
 )
+criaCartao(
+    'Volei',
+    'Quantos jogadores há em uma equipe de vôlei?',
+    'Uma equipe de vôlei de quadra é composta por seis jogadores, enquanto no vôlei de praia, são dois jogadores por equipe.'
+)
+
+criaCartao(
+    'Volei',
+    'Qual é a altura da rede no vôlei?',
+    'A altura da rede varia de acordo com o gênero: para jogos masculinos, a rede tem 2,43 metros de altura, e para jogos femininos, a altura é de 2,24 metros.'
+)
+
+criaCartao(
+    'Volei',
+    'Quais são os principais fundamentos do vôlei?',
+    'Os fundamentos básicos do vôlei são o saque, o passe (ou recepção), o levantamento, o ataque e o bloqueio.'
+)
